2c322150d4d070a2abd4d64327e567febe417121
arullk5566 <85780324+arullk5566@users.noreply.github.com>
2021-10-05 17:39:57 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -59,12 +59,6 @@ async def start_(client: Client, message: Message):
                     ),
                     InlineKeyboardButton(
                         "📣 Official Channel", url=f"https://t.me/{UPDATES_CHANNEL}")
-                ],[
-                    InlineKeyboardButton(
-                        "🌐 Wiki's Page", url="https://github.com/arullk5566/klimanam/wiki/Klimanam-Music-Wiki's")
-                ],[
-                    InlineKeyboardButton(
-                        "🧪 Source Code 🧪", url="https://github.com/arullk5566/Klimanam"
                     )
                 ]
             ]
